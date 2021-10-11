6065095c47ec017642c939ab94be523bde56d1b3
Ivan <akimovivan355@gmail.com>
2021-10-11 15:21:35 +0700

    new logkey function

--- src/tools/logkey.ts
+++ src/tools/logkey.ts
@@ -1,4 +1,4 @@
-import {IclientMouse, children, Istate} from '../App'
+import {IclientMouse, Istate} from '../App'
 
 
 interface props {
@@ -15,6 +15,55 @@ interface props {
 }
 
 
+
+
+const constructNewSateWithWhiteSpace = (e:React.MouseEvent, state: Istate[]) => {
+  const newState = [] as Istate[]
+  state.forEach((val, index) =>{
+    if(state[index].target === 1){
+      newState.push(state[index])
+      return
+    }
+    if(index+1 === state.length) {newState.push(state[index]); return}
+    if(val.header === 'white space') return
+    
+    if(e.clientX > val.left   &&  e.clientX < state[index+1].left ){  
+      newState.push(state[index])
+      newState.push({
+        header:'white space',
+        id:Math.floor(1000*Math.random()),
+        left: e.clientX,
+        target:null,
+        childrens:[{x:1,y:1,text:'', index:20}],
+        length:1
+      })
+    
+      return 
+    }
+    newState.push(val)
+
+  })
+  return newState
+}
+
+
+
+const moveBlock = (clientMouse: IclientMouse, basis: number, state :Istate[], e:React.MouseEvent, targetDiv:HTMLDivElement,UpdateBlockAction: Function ) => {
+  const Xposition = Number(clientMouse.clientX) - basis
+  targetDiv.style.position = 'absolute'
+  targetDiv.style.left = `${Xposition}px`
+  targetDiv.style.top =`${clientMouse.clientY}px`
+
+  const newState = constructNewSateWithWhiteSpace(e, state)
+  UpdateBlockAction(newState)
+}
+
+const moveAction = (clientMouse: IclientMouse, left: number, targetDivElemnt:HTMLDivElement) => {
+  targetDivElemnt.style.position = 'absolute'
+  const calc  = Number(clientMouse.clientX) - left
+  targetDivElemnt.style.left = `${calc}px`
+  targetDivElemnt.style.top =`${clientMouse.clientY}px`
+}
 
 export function logKey({
     flag,
@@ -33,68 +82,15 @@ return (e:React.MouseEvent) => {
       clientX:e.clientX.toString(),
       clientY:e.clientY.toString()
     }
-    // console.log('x ',e.clientX.toString())
-    // console.log('y', e.clientY.toString())
-    
     setClientMouse(obj)
     if(flag)
       {
         if(isBlockMoved){
-
-
-          
-          
-          const Xposition = Number(clientMouse.clientX) - basis
-
-          console.log('initPosition')
-
-          //console.log(initPosition)
-          
-          console.log('number')
-          console.log(Number(clientMouse.clientX))
-
-          console.log('basis')
-          //console.log(basis)
-
-          targetDiv.style.position = 'absolute'
-          targetDiv.style.left = `${Xposition}px`
-          targetDiv.style.top =`${clientMouse.clientY}px`
-
-          const obj = [] as Istate[] 
-          state.forEach((val, index) =>{
-            if(state[index].target === 1){
-              obj.push(state[index])
-              return
-            }
-            if(index+1 === state.length) {obj.push(state[index]); return}
-            if(val.header === 'white space') return
-            
-            if(e.clientX > val.left   &&  e.clientX < state[index+1].left ){  
-              obj.push(state[index])
-              obj.push({
-                header:'white space',
-                id:Math.floor(1000*Math.random()),
-                left: e.clientX,
-                target:null,
-                childrens:[{x:1,y:1,text:'', index:20}],
-                length:1
-              })
-            
-              return 
-            }
-            obj.push(val)
-
-          })
-          UpdateBlockAction(obj)
+          moveBlock(clientMouse, basis, state, e, targetDiv,UpdateBlockAction )
         }
         else{
-          // console.log('dddddddddddddddddddddddddddddddddd')
-          targetDivElemnt.style.position = 'absolute'
-          const calc  = Number(clientMouse.clientX) - left
+          moveAction(clientMouse, left, targetDivElemnt)
 
-
-          targetDivElemnt.style.left = `${calc}px`
-          targetDivElemnt.style.top =`${clientMouse.clientY}px`
         }
       }
 
